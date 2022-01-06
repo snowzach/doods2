9fd8fbd7e6f2d9a6329f2986d607d4687ac56db9
Zach Brown <git@prozach.org>
2022-01-06 09:07:45 -0500

    Move detection labels to bottom, fix label file processing

--- detectors/labels.py
+++ detectors/labels.py
@@ -6,13 +6,13 @@ def load_labels(path_to_labels):
     
     with open(path_to_labels, 'r') as File:
         file = File.readlines() #Reading all the lines from File
-        if not re.split(r'[\s\,]+', file[0])[0].isnumeric():
+        if not re.split(r'[\s\,]+', file[0], maxsplit=1)[0].isnumeric():
             index = 1
             for line in file: #Reading line-by-line
                 labels[index] = line.strip()
                 index = index + 1
         else:
             for line in file: #Reading line-by-line
-                words = re.split(r'[\s\,]+', line) #Splitting lines in words using space or comma character as separator
-                labels[int(words[0])] = words[1]
+                words = re.split(r'[\s\,]+', line, maxsplit=1) #Splitting lines in words using space or comma character as separator
+                labels[int(words[0].strip())] = words[1].strip()
     return labels
